1168a2dffb32040f61759d08e804c604f338df29
Paul Yang <wenboyang3-c@my.cityu.edu.hk>
2019-10-28 11:43:17 +0800

    add non-overriable poperty of functionamanger

--- Source/main/ctlogo/function/FunctionManager.java
+++ Source/main/ctlogo/function/FunctionManager.java
@@ -1,15 +1,18 @@
 package ctlogo.function;
 
 import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import ctlogo.exception.CTSyntaxException;
 import ctlogo.execute.expression.Expression;
 
 public class FunctionManager {
 	private Map<String, Function> functions;
+	private Set<String> nonOverridables;
 	
 	private static FunctionManager theInstance = new FunctionManager();
 	
@@ -19,10 +22,11 @@ public class FunctionManager {
 	
 	private FunctionManager() {
 		this.functions = new HashMap<>();
+		this.nonOverridables = new HashSet<>();
 	}
 	
     /**
-     * Register a function.
+     * Register a overrideable function.
      *
      * @param name name of the function
      * @param func body of the function
@@ -35,12 +39,33 @@ public class FunctionManager {
 		functions.put(name, func);
 	}
 	
+    /**
+     * Register a function.
+     *
+     * @param name name of the function
+     * @param func body of the function
+     * @throws IllegalArgumentException if function already registered.
+     */
+	public void register(String name, Function func, boolean isOerridable) {
+		if (functions.containsKey(name))
+            throw new IllegalArgumentException(
+                    "Registerring function twice not allowed.");
+		functions.put(name, func);
+		if (!isOerridable) 
+			nonOverridables.add(name);
+	}
+	
 	/**
 	 * @param name name of the function
 	 * @param func body of the function
 	 * @return whether the function has been registered before. 
+	 * @throws UnsupportedOperationException if the function is non-overridable
 	 */
 	public boolean reRegister(String name, Function func) {
+		if (nonOverridables.contains(name))
+			throw new UnsupportedOperationException(
+					"Cannot override non-overridenable function");
+
 		boolean alreadyExist = functions.containsKey(name);
 		functions.put(name, func);
 		return alreadyExist;
