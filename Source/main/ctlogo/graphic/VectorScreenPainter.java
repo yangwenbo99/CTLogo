b013d032d13a833e6fdcbeb38a6ea51ad017ff33
dennisCW <wong2844995490@gmail.com>
2019-11-10 22:06:17 +0800

    Follow the previous commit

--- Source/main/ctlogo/graphic/VectorScreenPainter.java
+++ Source/main/ctlogo/graphic/VectorScreenPainter.java
@@ -1,5 +1,13 @@
 package ctlogo.graphic;
 
-public class VectorScreenPainter {
+import java.awt.Graphics;
+
+public class VectorScreenPainter implements ScreenPainter {
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
