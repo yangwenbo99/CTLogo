194b9b49f3f9acd506e837ee45fc1ccc40ed38e1
Paul Yang <wenboyang3-c@my.cityu.edu.hk>
2019-11-21 16:37:00 +0800

    contrext

--- Source/main/ctlogo/execute/AbstractContext.java
+++ Source/main/ctlogo/execute/AbstractContext.java
@@ -13,33 +13,78 @@ public abstract class AbstractContext implements Context {
 	private PrintStream outputStream;
 	private Screen screen;
 	private VariableManager variableManager;
+	private Context enclosingContext;
 
-	public AbstractContext(Scanner scanner, PrintStream outputStream, Screen screen, VariableManager variableManager) {
+	/**
+	 * @param scanner
+	 * @param outputStream
+	 * @param screen
+	 * @param variableManager
+	 * @param enclContext
+	 * 
+	 * If {@code: enclosingContext} is not given, then all other parameters
+	 * cannot be null.
+	 */
+	public AbstractContext(
+			Scanner scanner,
+			PrintStream outputStream,
+			Screen screen,
+			VariableManager variableManager, 
+			Context enclContext) {
 		super();
 		this.scanner = scanner;
 		this.outputStream = outputStream;
 		this.screen = screen;
 		this.variableManager = variableManager;
+		this.enclosingContext = enclContext;
+		if (enclContext == null) {
+			if (
+					scanner == null || outputStream == null || 
+					screen == null || variableManager == null) {
+				throw new IllegalArgumentException(
+						"No context information can be null, unless it has an encloding context");
+			}
+		}
+	}
+
+	public AbstractContext(
+			Scanner scanner,
+			PrintStream outputStream,
+			Screen screen,
+			VariableManager variableManager) {
+		this(scanner, outputStream, screen, variableManager, null);
 	}
 
 	@Override
 	public Scanner getInputScanner() {
-		return scanner;
+		if (scanner != null)
+			return scanner;
+		else 
+			return enclosingContext.getInputScanner();
 	}
 
 	@Override
 	public PrintStream getOutputStream() {
-		return outputStream;
+		if (outputStream != null)
+			return outputStream;
+		else
+			return enclosingContext.getOutputStream();
 	}
 
 	@Override
 	public Screen getScreen() {
-		return screen;
+		if (screen != null) 
+			return screen;
+		else
+			return enclosingContext.getScreen();
 	}
 
 	@Override
 	public VariableManager getVariableManager() {
-		return variableManager;
+		if (variableManager != null)
+			return variableManager;
+		else
+			return enclosingContext.getVariableManager();
 	}
 
 	/**
@@ -49,7 +94,7 @@ public abstract class AbstractContext implements Context {
 	 */
 	@Override
 	public CTValue getValueOf(String vname) {
-		return variableManager.getValue(vname);
+		return getVariableManager().getValue(vname);
 	}
 
 }
