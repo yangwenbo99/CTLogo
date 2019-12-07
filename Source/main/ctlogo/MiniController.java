5efd26a221ca7d3d7a2ca645a80451e36a541024
Paul Yang <yangwenbo99@gmail.com>
2019-12-07 14:13:33 +0800

    controller update

--- Source/main/ctlogo/MiniController.java
+++ Source/main/ctlogo/MiniController.java
@@ -48,57 +48,6 @@ public class MiniController {
 				super(scanner, outputStream, screen, variableManager);
 			}
 		}
-		
-//		class StubScreen implements Screen {
-//
-//			@Override
-//			public void drawLine(double x1, double y1, double x2, double y2) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void drawRectangle(double x1, double y1, double x2, double y2) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void drawEclipse(double cx, double cy, double a, double b) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void clean() {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void setWidth(double w) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void setHeight(double h) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void setStroke(double s) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void setColor(String color) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		}
 
 		Context stubContext = new StubContext(
 				new Scanner("Test scanner"), 
